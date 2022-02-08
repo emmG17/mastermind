d5df0f641629b82174f99c80ab82f109a14b4bf3
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-02-08 12:53:23 -0600

    Remove unused array declaration

--- mastermind.rb
+++ mastermind.rb
@@ -1,27 +1,22 @@
-pegs = ["r","b","g","y", "o","p"]
-
-def new_code (pegs)
+def new_code
+  pegs = %w[r b g y o p]
   pegs.sample(4)
 end
 
-def find_partial_matches (guess, code)
-  res = code.each_with_index.select { |peg, i|  guess.find_index(peg) && peg != guess[i] }
-  res
+def find_partial_matches(guess, code)
+  code.each_with_index.select { |peg, i| guess.find_index(peg) && peg != guess[i] }
 end
 
-def find_exact_matches (guess, code)
-  res = code.each_with_index.select { |peg, i| peg == guess[i] }
-  res
+def find_exact_matches(guess, code)
+  code.each_with_index.select { |peg, i| peg == guess[i] }
 end
 
-def hints (exact_matches, partial_matches)
-  exact_hint = "■"
-  partial_hint = "¤"
+def hints(exact_matches, partial_matches)
+  exact_hint = '■'
+  partial_hint = '¤'
 
   hints = Array.new(4, '-')
-  exact_matches.each { |match| hints[match[1]] = exact_hint}
-  partial_matches.each { |match| hints[match[1]] = partial_hint}
+  exact_matches.each { |match| hints[match[1]] = exact_hint }
+  partial_matches.each { |match| hints[match[1]] = partial_hint }
   hints
 end
-
-
